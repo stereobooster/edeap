4b9fafec86cf89234059029fc0ed0ddaebfadbd3
stereobooster <stereobooster@gmail.com>
2024-05-27 14:42:36 +0200

    refactoring

--- src/pure.ts
+++ src/pure.ts
@@ -520,3 +520,26 @@ export function nextGridPoint(point: Point) {
     y: nextGridValue(point.y),
   };
 }
+
+// Bit masks for different types of logging.
+// Each should have a value of "2 ** n" where n is next value.
+// const logNothing = 0;
+export const logFitnessDetails = 2 ** 0;
+export const logOptimizerStep = 2 ** 1;
+export const logOptimizerChoice = 2 ** 2;
+export const logReproducability = 2 ** 3;
+
+// Select the type of logging to display.  To select multiple types
+// of logging, assign this variable a value via options separated by
+// bitwise OR (|):
+//    showLogTypes = logReproducability | logOptimizerStep;
+const showLogTypes = logReproducability;
+
+// Function to be able to disable fitness logging.
+export function logMessage(type: number, ..._messages: any[]) {
+  if (showLogTypes & type) {
+    const args = Array.prototype.slice.call(arguments);
+    args.shift();
+    console.log.apply(console, args);
+  }
+}
--- src/ui.ts
+++ src/ui.ts
@@ -1,4 +1,4 @@
-import { generateRandomZones } from "./pure";
+import { colourPalettes, decodeAbstractDescription, generateRandomZones } from "./pure";
 
 // downloadFileFromText function from:
 // https://stackoverflow.com/questions/4845215/making-a-chrome-extension-download-a-file
@@ -43,7 +43,7 @@ export function getDownloadName() {
   );
 }
 
-export function saveAreaSpecification() {
+function saveAreaSpecification() {
   const areaSpecificationString = (
     document.getElementById("areaSpecification") as HTMLTextAreaElement
   ).value;
@@ -78,3 +78,95 @@ export const heightForSvgDownload = () => {
   if (isNaN(height)) return canvasHeight();
   return height;
 };
+
+export function initUI({
+  colourPaletteName,
+  startingDiagram,
+  areaSpecificationText,
+  optimizationMethod,
+}: {
+  colourPaletteName: string;
+  startingDiagram: string;
+  areaSpecificationText: string;
+  optimizationMethod: string;
+}) {
+  document
+    .getElementById("areaSpecDownload")
+    ?.addEventListener("click", saveAreaSpecification);
+  document
+    .getElementById("generateRandomDiagram")
+    ?.addEventListener("click", generateRandomDiagram);
+
+  const palette = document.getElementById("palette") as HTMLSelectElement;
+  // Add colour palette options to HTML select element.
+  for (const paletteName in colourPalettes) {
+    const option = document.createElement("option");
+    option.text = paletteName;
+    palette.add(option);
+  }
+  // Select the chosen colour palette.
+  for (let i = 0; i < palette.length; i++) {
+    if (colourPaletteName == palette.options[i].text) {
+      palette.selectedIndex = i;
+    }
+  }
+
+  const filePickerRef = document.getElementById(
+    "areaSpecFilePicker"
+  ) as HTMLInputElement;
+
+  filePickerRef.addEventListener("change", function (event) {
+    const reader = new FileReader();
+
+    // Setup completion callback for FileReader object.
+    reader.onload = function (event) {
+      // Get the text from the file and show it in the outputArea div.
+      const fileText = event.target?.result;
+      (document.getElementById("areaSpecification") as HTMLInputElement).value =
+        fileText?.toString() || "";
+    };
+
+    // Tell the FileReader to start reading the file.
+    // @ts-expect-error
+    const file = event.target.files[0];
+    reader.readAsText(file);
+  });
+
+  document.getElementById("downloadName")!.innerHTML =
+    getDownloadName() + ".svg";
+
+  const widthEntry = document.getElementById("widthEntry") as HTMLInputElement;
+  widthEntry.value = String(widthForSvgDownload());
+
+  const heightEntry = document.getElementById(
+    "heightEntry"
+  ) as HTMLInputElement;
+  heightEntry.value = String(heightForSvgDownload());
+
+  if (startingDiagram === "random") {
+    (document.getElementById("startingDefault") as HTMLInputElement).checked =
+      false;
+    (document.getElementById("startingRandom") as HTMLInputElement).checked =
+      true;
+  } else {
+    (document.getElementById("startingDefault") as HTMLInputElement).checked =
+      true;
+    (document.getElementById("startingRandom") as HTMLInputElement).checked =
+      false;
+  }
+
+  document.getElementById("areaSpecification")!.innerHTML =
+    decodeAbstractDescription(areaSpecificationText);
+
+  if (optimizationMethod === "1" || optimizationMethod === "") {
+    (document.getElementById("optimizationHill") as HTMLInputElement).checked =
+      true;
+    (document.getElementById("optimizationSE") as HTMLInputElement).checked =
+      false;
+  } else {
+    (document.getElementById("optimizationHill") as HTMLInputElement).checked =
+      false;
+    (document.getElementById("optimizationSE") as HTMLInputElement).checked =
+      true;
+  }
+}
