95a98942d74e28401bc8d11c78000ead75b880f3
stereobooster <stereobooster@gmail.com>
2024-05-27 19:08:53 +0200

    refactoring

--- src/venneulerstress.ts
+++ src/venneulerstress.ts
@@ -30,14 +30,6 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-// type EllipseParams = {
-//   A: number;
-//   B: number;
-//   R: number;
-//   X: number;
-//   Y: number;
-// };
-
 // class VennEulerAreas {
 //   ellipseLabels: string[];
 //   globalZoneStrings: string[];
