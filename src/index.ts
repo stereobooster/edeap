f2d06e356fad0bae3aa3440f99419db9b89aa5bb
stereobooster <stereobooster@gmail.com>
2024-05-30 18:15:44 +0200

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { EdeapAreas } from "./EdeapAreas";
 import { Optimizer } from "./optimizer";
 import { generateSVG, initialState } from "./other";
 import { Config, OptimizerConfig, SetOverlaps, State } from "./types";
@@ -16,8 +17,48 @@ type SVGConfig = {
 
 export class Edeap {
   state: State;
+  areas: EdeapAreas;
   constructor(config: Config) {
     this.state = initialState(config);
+    this.areas = new EdeapAreas(this.state);
+
+    // reproducability logging
+    // logMessage(
+    //   logReproducability,
+    //   "// paste this into the abstract description:"
+    // );
+    // logMessage(logReproducability, overlaps);
+    // logMessage(
+    //   logReproducability,
+    //   "// paste this in index.html just before the reproducability logging:"
+    // );
+    // for (let i = 0; i < sharedState.ellipseParams.length; i++) {
+    //   logMessage(logReproducability, `ellipseParams[${i}] = {};`);
+    //   logMessage(
+    //     logReproducability,
+    //     "ellipseParams[" + i + "].X = " + sharedState.ellipseParams[i].X + ";"
+    //   );
+    //   logMessage(
+    //     logReproducability,
+    //     "ellipseParams[" + i + "].Y = " + sharedState.ellipseParams[i].Y + ";"
+    //   );
+    //   logMessage(
+    //     logReproducability,
+    //     "ellipseParams[" + i + "].A = " + sharedState.ellipseParams[i].A + ";"
+    //   );
+    //   logMessage(
+    //     logReproducability,
+    //     "ellipseParams[" + i + "].B = " + sharedState.ellipseParams[i].B + ";"
+    //   );
+    //   logMessage(
+    //     logReproducability,
+    //     "ellipseParams[" + i + "].R = " + sharedState.ellipseParams[i].R + ";"
+    //   );
+    //   logMessage(
+    //     logReproducability,
+    //     "ellipseLabel[" + i + "] = '" + sharedState.ellipseLabel[i] + "';"
+    //   );
+    // }
   }
   optimizie(config: OptimizerConfig) {
     const opt = new Optimizer({
@@ -35,5 +76,8 @@ export class Edeap {
       showValues === undefined ? true : showValues,
       standalone === undefined ? false : standalone
     );
+  }
+  htmlReport() {
+    return this.areas.zoneAreaTableBody();
   }
 }
